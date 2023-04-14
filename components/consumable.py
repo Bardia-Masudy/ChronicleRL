0a7a6b278ef854a11533c63c02ab1dbab4a6aa96
Bardia Masudy <bmasudy74@gmail.com>
2023-04-14 11:14:07 -0700

    part 9 done, scrolls, ranged attack input, new ai

--- components/consumable.py
+++ components/consumable.py
@@ -5,8 +5,10 @@ from typing import Optional, TYPE_CHECKING
 import actions
 import colour
 import components.inventory
+import components.ai
 from components.base_component import BaseComponent
 from exceptions import Impossible
+from input_handlers import AreaRangedAttackHandler, SingleRangedAttackHandler
 
 if TYPE_CHECKING:
     from entity import Actor, Item
@@ -30,6 +32,75 @@ class Consumable(BaseComponent):
         if isinstance(inventory, components.inventory.Inventory):
             inventory.items.remove(entity)
 
+class ConfusionConsumable(Consumable):
+    def __init__(self, duration: int) -> None:
+        self.duration = duration
+
+    def get_action(self, consumer: Actor) -> Optional[actions.Action]:
+        self.engine.message_log.add_message(
+            "Select a target location.", colour.needs_target
+        )
+        self.engine.event_handler = SingleRangedAttackHandler(
+            self.engine,
+            callback=lambda xy: actions.ItemAction(consumer, self.parent, xy)
+        )
+        return None
+    
+    def activate(self, action: actions.ItemAction) -> None:
+        consumer = action.entity
+        target = action.target_actor
+
+        if not self.engine.game_map.visible[action.target_xy]:
+            raise Impossible("You cannot target an area you cannot see.")
+        if not target:
+            raise Impossible("You must select an enemy to target.")
+        if target is consumer:
+            raise Impossible("You cannot confuse yourself!") #TODO: make this possible, add confusion player status
+        
+        self.engine.message_log.add_message(
+            f"The {target.name}'s stare grows vacant, as it starts stumbling around!",
+            colour.status_effect_applied
+        )
+        target.ai = components.ai.ConfusedEnemy(
+            entity=target, previous_ai=target.ai, duration=self.duration
+        )
+        self.consume()
+
+class FireballDamageConsumable(Consumable):
+    def __init__(self, damage: int, radius: int):
+        self.damage = damage
+        self.radius = radius
+
+    def get_action(self, consumer: Actor) -> Optional[actions.Action]:
+        self.engine.message_log.add_message(
+            "Select a target location.", colour.needs_target
+        )
+        self.engine.event_handler = AreaRangedAttackHandler(
+            self.engine,
+            radius= self.radius,
+            callback= lambda xy: actions.ItemAction(consumer, self.parent, xy)
+        )
+        return None
+    
+    def activate(self, action: actions.ItemAction) -> None:
+        target_xy = action.target_xy
+
+        if not self.engine.game_map.visible[target_xy]:
+            raise Impossible("You cannot target an area that you cannot see.")
+        
+        targets_hit = False
+        for actor in self.engine.game_map.actors:
+            if actor.distance(*target_xy) <= self.radius:
+                self.engine.message_log.add_message(
+                    f"The {actor.name} is engulfed in the firey explosion, taking {self.damage} damage!"
+                )
+                actor.fighter.take_damage(self.damage)
+                targets_hit = True
+        
+        if not targets_hit:
+            raise Impossible("There are no targets in the radius.") # TODO: change this to a better message, maybe a confirmation ask.
+        self.consume()
+
 class HealingConsumable(Consumable):
     def __init__(self, amount: int):
         self.amount = amount
@@ -46,3 +117,28 @@ class HealingConsumable(Consumable):
             self.consume()
         else:
             raise Impossible(f"Your health is already full.")
+    
+class LightningDamageConsumable(Consumable):
+    def __init__(self, damage: int, max_range: int):
+        self.damage = damage
+        self.max_range = max_range
+    
+    def activate(self, action: actions.ItemAction) -> None:
+        consumer = action.entity
+        target = None
+        closest_distance = self.max_range + 1.0
+
+        for actor in self.engine.game_map.actors:
+            if actor is not consumer and self.parent.gamemap.visible[actor.x, actor.y]:
+                distance = consumer.distance(actor.x, actor.y)
+
+                if distance < closest_distance:
+                    target = actor
+                    closest_distance = distance
+
+        if target:
+            self.engine.message_log.add_message(f"A bolt of lightning strikes the {target.name} with a roar of thunder, dealing {self.damage} damage!")
+            target.fighter.take_damage(self.damage)
+            self.consume()
+        else:
+            raise Impossible("Lightning crackles in the air, but there's no valid target close enough.")
--- input_handlers.py
+++ input_handlers.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import Optional
 
 
-from typing import Optional, TYPE_CHECKING
+from typing import Callable, Optional, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from engine import Engine
@@ -55,6 +55,11 @@ WAIT_KEYS = {
     tcod.event.K_PERIOD,
     tcod.event.K_KP_5,
     tcod.event.K_CLEAR,
+}
+
+CONFIRM_KEYS = {
+    tcod.event.K_RETURN,
+    tcod.event.K_KP_ENTER
 }
 class EventHandler(tcod.event.EventDispatch[Action]):
     def __init__(self, engine: Engine):
@@ -115,7 +120,7 @@ class MainGameEventHandler(EventHandler):
         elif key == tcod.event.K_ESCAPE:
             raise SystemExit()
         
-        elif key == tcod.event.K_v:
+        elif key == tcod.event.K_m:
             self.engine.event_handler = HistoryViewer(self.engine)
         
         elif key == tcod.event.K_g:
@@ -125,6 +130,8 @@ class MainGameEventHandler(EventHandler):
             self.engine.event_handler = InventoryActivateHandler(self.engine)
         elif key == tcod.event.K_d:
             self.engine.event_handler = InventoryDropHandler(self.engine)
+        elif key == tcod.event.K_v:
+            self.engine.event_handler = LookHandler(self.engine)
         
         # No valid keys returned
         return action
@@ -244,6 +251,109 @@ class InventoryDropHandler(InventoryEventHandler):
     def on_item_selected(self, item: Item) -> Optional[Action]:
         """Drop this item."""
         return actions.DropItem(self.engine.player, item)
+
+class SelectIndexHandler(AskUserEventHandler):
+    """Handles asking the user for an index on the map."""
+    def __init__(self, engine: Engine):
+        """Sets the cursor to the player's location."""
+        super().__init__(engine)
+        player = self.engine.player
+        engine.mouse_xy = player.x, player.y
+    
+    def on_render(self, console: tcod.Console) -> None:
+        """Highlight the tile under the cursor."""
+        super().on_render(console)
+        x, y = self.engine.mouse_xy
+        console.tiles_rgb["bg"][x, y] = colour.white
+        console.tiles_rgb["fg"][x, y] = colour.black
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+        """Check for key movement or confirmation keys."""
+        key = event.sym
+        if key in MOVE_KEYS:
+            modifier = 1 # Holding modifier keys will spped up cursor movement.
+            if event.mod & (tcod.event.KMOD_LSHIFT | tcod.event.KMOD_RSHIFT):
+                modifier *= 5
+            if event.mod & (tcod.event.KMOD_LCTRL | tcod.event.KMOD_RCTRL):
+                modifier *= 10
+            if event.mod & (tcod.event.KMOD_LALT | tcod.event.KMOD_RALT):
+                modifier *= 20
+
+            x, y = self.engine.mouse_xy
+            dx, dy = MOVE_KEYS[key]
+            x += dx * modifier
+            y += dy * modifier
+            # Clamp cursor index to map size
+            x = max(0, min(x, self.engine.game_map.width - 1))
+            y = max(0, min(y, self.engine.game_map.height - 1))
+            self.engine.mouse_xy = x, y
+            return None
+        elif key in CONFIRM_KEYS:
+            return self.on_index_selected(*self.engine.mouse_xy)
+        return super().ev_keydown(event)
+
+    def ev_mousebuttondown(self, event: tcod.event.MouseButtonDown) -> Optional[Action]:
+        """Left click confirms a selection."""
+        if self.engine.game_map.in_bounds(*event.tile):
+            if event.button == 1:
+                return self.on_index_selected(*event.tile)
+        return super().ev_mousebuttondown(event)
+
+    def on_index_selected(self, x: int, y: int) -> Optional[Action]:
+        """Called when an index is selected."""
+        raise NotImplementedError()
+    
+class LookHandler(SelectIndexHandler):
+    """Lets the player look around using the keyboard."""
+
+    def on_index_selected(self, x: int, y: int) -> None:
+        """Return to main handler."""
+        self.engine.event_handler = MainGameEventHandler(self.engine)
+
+class SingleRangedAttackHandler(SelectIndexHandler):
+    """Handles targeting a single enemy. Only the selected enemy will be affected."""
+
+    def __init__(self, engine: Engine, callback: Callable[[tuple[int, int]], Optional[Action]]):
+        super().__init__(engine)
+
+        self.callback = callback
+
+    def on_index_selected(self, x: int, y: int) -> Optional[Action]:
+        return self.callback((x, y))
+    
+class AreaRangedAttackHandler(SelectIndexHandler):
+    """Handles targeting an area ithin a given radius. Any entity within will be affected."""
+
+    def __init__(
+        self,
+        engine: Engine,
+        radius: int,
+        callback: Callable[[tuple[int, int]], Optional[Action]]
+    ):
+        super().__init__(engine)
+
+        self.radius = radius
+        self.callback = callback
+
+    def on_render(self, console: tcod.Console) -> None:
+        """Highlight the tile under the cursor."""
+        super().on_render(console)
+
+        x, y = self.engine.mouse_xy
+
+        # Draw a rectangle around the targets area, so the player can see
+        # TODO: use tcod djinkstra mapping or circle template for targetting, or as optional parameters
+        console.draw_frame(
+            x= x - self.radius - 1,
+            y= y - self.radius - 1,
+            width= self.radius ** 2,
+            height= self.radius ** 2,
+            fg= colour.red,
+            clear= False
+        )
+
+    def on_index_selected(self, x: int, y: int) -> Optional[Action]:
+        return self.callback((x, y))
 class GameOverEventHandler(EventHandler):
     def ev_keydown(self, event: tcod.event.KeyDown) -> None:
         if event.sym == tcod.event.K_ESCAPE:
